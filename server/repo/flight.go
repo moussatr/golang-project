18bdddc49752e4de734d918ef92c834e28d49d32
Moussa Traore <moussatraore@MacBook-Pro-de-Moussa.local>
2025-09-24 15:49:45 +0200

    add more attribut in Flight struct

--- server/repo/flight.go
+++ server/repo/flight.go
@@ -4,6 +4,10 @@ import "time"
 
 type Flight struct {
 	BokingId         string    `json:"bookingId"`
+	Reference        string    `json:"reference"`
+	Traveler         traveler  `json:"traveler"`
+	Total            total     `json:"total"`
+	Segments         []segment `json:"segments"`
 	Status           string    `json:"status"`
 	PassengerName    string    `json:"passengerName"`
 	FlightNumber     string    `json:"flightNumber"`
@@ -13,4 +17,26 @@ type Flight struct {
 	ArrivalTime      time.Time `json:"arrivalTime"`
 	Price            float64   `json:"price"`
 	Currency         string    `json:"currency"`
+}
+
+type traveler struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+type total struct {
+	Amount   float64 `json:"amount"`
+	Currency string  `json:"currency"`
+}
+
+type segment struct {
+	Flight flight `json:"flight"`
+}
+
+type flight struct {
+	Number string    `json:"number"`
+	From   string    `json:"from"`
+	To     string    `json:"to"`
+	Depart time.Time `json:"depart"`
+	Arrive time.Time `json:"arrive"`
 }
